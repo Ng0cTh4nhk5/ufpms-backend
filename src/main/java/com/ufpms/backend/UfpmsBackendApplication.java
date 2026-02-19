a764307bfee9ac5ca4ed0948586d02d454a42a1d
Thanh Dang <dangn.thanh2005@gmail.com>
2026-02-19 09:04:50 +0700

    test: try pushing directly to main

---
+++ src/main/java/com/ufpms/backend/UfpmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ufpms.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfpmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfpmsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufpms/backend/UfpmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpms.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfpmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
